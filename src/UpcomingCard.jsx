c14f24e7cd63fe94de5c835830cf91e7ab103c90
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-03-28 10:07:47 +0600

    Upcoming section completed

--- src/UpcomingCard.jsx
+++ src/UpcomingCard.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const UpcomingCard = ({ cardData }) => {
-  return <div>This is upcoming card</div>;
+  return (
+    <div className="border-2 py-5 px-4 h-full rounded-2xl bg-green-50">
+      <h2 className="text-gray-600 mb-4">
+        <span className="font-bold text-xl">{cardData.category}</span>
+      </h2>
+      <div className="text-gray-600">
+        <span className="font-semibold">{cardData.bookName}</span>
+      </div>
+      <div className="text-gray-600">
+        <span className="font-semibold">By: </span>
+        {cardData.writerName}
+      </div>
+      <div className="text-gray-600">
+        <span className="font-semibold">Publisher Year: </span>
+        {cardData.publishedDate}
+      </div>
+      <div className="text-gray-600">
+        <span className="font-semibold">Rating: </span>
+        {cardData.rating}
+      </div>
+      <div className="text-gray-600">
+        <span className="font-semibold">Publish Date: </span>
+        {cardData.publishedDate}
+      </div>
+      <div className="text-gray-600">
+        <span className="font-semibold">Relase Date: </span>{" "}
+        {cardData.releaseDate}
+      </div>
+    </div>
+  );
 };
 
 export default UpcomingCard;
--- src/Upcomingsection.jsx
+++ src/Upcomingsection.jsx
@@ -5,11 +5,20 @@ const Upcomingsection = ({ data }) => {
   return (
     <div>
       <div>
-        <div>{data.categoryName}</div>
-        <div>{data.description}</div>
-        {/* {data.map((cardData, index) => (
-          <UpcomingCard key={2293 + index} cardData={cardData}></UpcomingCard>
-        ))} */}
+        <div className="mx-10 md:mx-32 lg:mx-40 text-2xl md:text-3xl text-gray-400 font-bold lg:text-4xl mt-8">
+          {data.categoryName}
+        </div>
+        <div className="mx-10 md:mx-32 lg:mx-40 font-bold my-4">
+          {data.description}
+        </div>
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center mx-10 md:mx-32 lg:mx-40">
+          {data.books.map((cardData, index) => (
+            <UpcomingCard
+              key={data.categoryName + index}
+              cardData={cardData}
+            ></UpcomingCard>
+          ))}
+        </div>
       </div>
     </div>
   );
